32d30652bf058070d21a837bb1340178ac5be779
Marcel Puyat <marcelp@stanford.edu>
2015-05-15 15:26:36 -0700

    Actually remove deleted fils

--- event_page/Note.js
+++
@@ -1,8 +0,0 @@
-// Struct to hold all fields needed for a popup notif
-function Note(provider, id, title, url) {
-	this.provider = provider;
-	// Universal id is provider_id. (i.e. pocket_121, evernote_1251, etc.)
-	this.id = provider + "_" + id;
-	this.title = title;
-	this.url = url;
-}
